7cadd95b44e902d527c953a47ce8e0da0b1410c3
Joseph Mennemeier <jmennemeier@aol.com>
2017-02-13 14:00:19 -0600

    Initial commit, creating views, tables, and files for effects

---
+++ BudgetSync/BillsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BillsTableViewCell.swift
+//  BudgetSync
+//
+//  Created by Joseph Mennemeier on 2/13/17.
+//  Copyright © 2017 Joseph Mennemeier. All rights reserved.
+//
+
+import UIKit
+
+class BillsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
